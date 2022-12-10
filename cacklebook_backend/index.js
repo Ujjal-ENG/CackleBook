a5ec21990bd6379d04d0cd6bb53a266824098319
user <ujjalroy7862@gmail.com>
2022-12-10 21:55:16 +0600

    add the middle ware for verification purpose

--- cacklebook_backend/index.js
+++ cacklebook_backend/index.js
@@ -8,7 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import { register } from "./controllers/auth.js";
+import  authRoutes  from "./routes/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -37,8 +39,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// SETUP ROUTE WITH FILES
+// SETUP WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
 
 //MONGOOSE SETUP
 
